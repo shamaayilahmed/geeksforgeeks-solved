8ff646a3aedee2ba07b01a61d89b3e4a98bc1b79
Shamaayil ahmed <55784371+shamaayilahmed@users.noreply.github.com>
2020-04-29 13:24:39 +0530

    Update Rearrange array alternatively.cpp

--- Rearrange array alternatively.cpp
+++ Rearrange array alternatively.cpp
@@ -1,7 +1,12 @@
-#include <vector>
-#include<algorithm>
 #include<bits/stdc++.h>
 using namespace std;
+/* GIVEN AN SORTED ARRAY REARRANGE IN MAX-MIN FASHION
+  SAMPLE
+  1               test case
+  6               size of array
+  1 2 3 4 5 6     input array
+  6 1 5 2 4 3     output first max 6,first min 1,so on..
+*/
 
 int main()
 {
@@ -22,6 +27,8 @@ int main()
         for(i=0;i<l/2;i++){
         cout<<a[l-1-i]<<" "<<a[i]<<" ";
         }
+        if((l%2)!=0)
+            cout<<a[l/2];
         cout<<"\n";
     }
     return 0;
